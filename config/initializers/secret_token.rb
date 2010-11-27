1f4acb42b44bf71527de28afd2d4e0f2f0198255
Artur Rybak <artur.rybak@gmail.com>
2010-11-27 15:08:50 +0100

    Kontakty od nowa

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Kontakty::Application.config.secret_token = '8b2f0bcfaca177746737f5a297b7242aed50760643523baac05bb2383560b7cdb685699f2aeb143fcc8af695a0f5d1609393e750e6a79fa03dea654e62eea506'
