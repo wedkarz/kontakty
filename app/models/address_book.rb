15b15b03bc3238ce3e0bba36fbd6edadff15e346
Artur Rybak <artur.rybak@gmail.com>
2010-11-27 15:45:36 +0100

    Zaktualizowano model oraz dodano podstawowe walidatory

--- app/models/address_book.rb
+++ app/models/address_book.rb
@@ -1,2 +1,4 @@
 class AddressBook < ActiveRecord::Base
+  validates :imie, :nazwisko, :format => { :with => /^[a-z]+$/i }, :presence => true
+  validates :email, :format => { :with => /^([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})$/i }, :presence => true, :uniqueness => true
 end
